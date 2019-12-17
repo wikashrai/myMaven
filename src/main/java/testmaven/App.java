ac70fddfb906d520e089c553a7474b6d1cbed6bd
Bikash Rai <Bikash.rai@LPT1463.newgen.co.in>
2019-12-17 18:09:31 +0530

    test

---
+++ src/main/java/testmaven/App.java
@@ -0,0 +1,14 @@
+package testmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println("asdfasdfasdfsdf");
+        System.out.println( "Hello World!" );
+    }
+}
